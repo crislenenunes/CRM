b88ff11ec9cbd271f02c774677788d6a2fb43505
Crislene Nunes <156555452+crislenenunes@users.noreply.github.com>
2025-04-23 22:41:11 -0300

    Create database.py

---
+++ backend/database.py
@@ -0,0 +1,14 @@
+import json
+
+DATA_FILE = "data/leads.json"
+
+def load_data():
+    try:
+        with open(DATA_FILE, 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
+
+def save_data(data):
+    with open(DATA_FILE, 'w') as file:
+        json.dump(data, file, indent=4)
